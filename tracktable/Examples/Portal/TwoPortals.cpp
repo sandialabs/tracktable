9807a9eac25df27f1a8884c564d9b86673212c26
Andy Wilson <atwilso@sandia.gov>
2017-10-01 14:21:55 -0700

    Update copyright for NTESS transition

--- tracktable/Examples/Portal/TwoPortals.cpp
+++ tracktable/Examples/Portal/TwoPortals.cpp
@@ -1,10 +1,16 @@
+/*
+ * Copyright (c) 2014-2017 National Technology and Engineering
+ * Solutions of Sandia, LLC. Under the terms of Contract DE-NA0003525
+ * with National Technology and Engineering Solutions of Sandia, LLC,
+ * the U.S. Government retains certain rights in this software.
+ */
+
 //
 //   TwoPortals
 //   
 // First example of finding two portals in the flight data
 //
 // Created by Danny Rintoul
-// Copyright (c) 2014 Sandia Corporation.  All rights reserved.
 //
 
 #include "TwoPortals.h"
