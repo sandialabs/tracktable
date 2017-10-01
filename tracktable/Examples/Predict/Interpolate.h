8e7f43ab9ee6217eba44e34292ab7790c5eb6af2
Andy Wilson <atwilso@sandia.gov>
2017-10-01 14:31:18 -0700

    Update copyright for NTESS transition

--- tracktable/Examples/Predict/Interpolate.h
+++ tracktable/Examples/Predict/Interpolate.h
@@ -1,11 +1,16 @@
-// 
+/*
+ * Copyright (c) 2014-2017 National Technology and Engineering
+ * Solutions of Sandia, LLC. Under the terms of Contract DE-NA0003525
+ * with National Technology and Engineering Solutions of Sandia, LLC,
+ * the U.S. Government retains certain rights in this software.
+ */
+
 // Interpolate
 //
 // Do Interpolations
 //
 //
 // Created by Danny Rintoul
-// Copyright (c) 2015 Sandia Corporation.  All rights reserved.
 //
 
 #ifndef __FindSimilar
