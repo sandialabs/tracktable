a303fa2c6e0394296b77440c83bd442b5625dc36
Andy Wilson <atwilso@sandia.gov>
2017-10-01 14:21:55 -0700

    Update copyright for NTESS transition

--- tracktable/Examples/Portal/SimilarShape.h
+++ tracktable/Examples/Portal/SimilarShape.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2013-2017 National Technology and Engineering
+ * Solutions of Sandia, LLC. Under the terms of Contract DE-NA0003525
+ * with National Technology and Engineering Solutions of Sandia, LLC,
+ * the U.S. Government retains certain rights in this software.
+ */
+
 // 
 // Support
 //
@@ -5,7 +12,6 @@
 //
 //
 // Created by Danny Rintoul
-// Copyright (c) 2013 Sandia Corporation.  All rights reserved.
 //
 
 #ifndef __Support
--- tracktable/Examples/Portal/Support.cpp
+++ tracktable/Examples/Portal/Support.cpp
@@ -1,10 +1,16 @@
+/*
+ * Copyright (c) 2013-2017 National Technology and Engineering
+ * Solutions of Sandia, LLC. Under the terms of Contract DE-NA0003525
+ * with National Technology and Engineering Solutions of Sandia, LLC,
+ * the U.S. Government retains certain rights in this software.
+ */
+
 //
 //   Supporting tools
 //   
 // A simple example how one can find similar trajectorys using control points
 //
 // Created by Danny Rintoul
-// Copyright (c) 2013 Sandia Corporation.  All rights reserved.
 //
 
 #include <numeric>
