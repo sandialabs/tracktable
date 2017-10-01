94eed0205ea90472d7bdc6f4bc3fbbf6017b21fc
Andy Wilson <atwilso@sandia.gov>
2017-10-01 15:44:18 -0700

    Update copyright for NTESS transition

--- tracktable/Python/tracktable/script_helpers/argument_groups/trajectory_reader.py
+++ tracktable/Python/tracktable/script_helpers/argument_groups/trajectory_reader.py
@@ -1,7 +1,10 @@
-# Copyright (c) 2014, Sandia Corporation.  All rights
-# reserved.
 #
-# Redistribution and use in source and binary forms, with or without
+# Copyright (c) 2014-2017 National Technology and Engineering
+# Solutions of Sandia, LLC. Under the terms of Contract DE-NA0003525
+# with National Technology and Engineering Solutions of Sandia, LLC,
+# the U.S. Government retains certain rights in this software.
+#
+# Distribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
 #
