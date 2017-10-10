e1dfd5960cd05e217deb194f5f71d50eb5b61043
Andy Wilson <atwilso@sandia.gov>
2017-10-10 16:47:01 -0600

    Work around boost::bind disagreement with fabs
    Apparently it is tricky to get fabs to work with boost::bind.  This
    hotfix works around the problem by wrapping the fabs stuff in another
    function that boost::bind can work with gracefully.
    
    Must find out what the problem really is since fabs has given us
    headaches across platforms.

--- tracktable/Examples/Classify/Mapping.h
+++ tracktable/Examples/Classify/Mapping.h
@@ -9,8 +9,8 @@
 #define _Mapping_h_
 #include "Common.h"
 
-unsigned int TurnArounds(trajectory_type &trajectory);
-double StraightFraction(trajectory_type &trajectory);
+unsigned int TurnArounds(trajectory_type const& trajectory);
+double StraightFraction(trajectory_type const& trajectory);
 double HeadingDifference(const double h2, const double h1);
 double TrajHeadingDifference(const trajectory_point_type &t2,
  const trajectory_point_type &t1);
