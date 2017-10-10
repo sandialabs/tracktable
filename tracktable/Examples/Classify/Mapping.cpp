44aabfe3943cf3cbe3043a3d0a9a4e5d89fffcdd
Andy Wilson <atwilso@sandia.gov>
2017-10-10 16:47:01 -0600

    Work around boost::bind disagreement with fabs
    Apparently it is tricky to get fabs to work with boost::bind.  This
    hotfix works around the problem by wrapping the fabs stuff in another
    function that boost::bind can work with gracefully.
    
    Must find out what the problem really is since fabs has given us
    headaches across platforms.

--- tracktable/Examples/Classify/Mapping.cpp
+++ tracktable/Examples/Classify/Mapping.cpp
@@ -14,73 +14,15 @@
 #include <algorithm>
 #include <boost/bind.hpp>
 
-unsigned int TurnArounds(trajectory_type &trajectory)
-{
-  const unsigned int window = 5;
-  unsigned int ctr = 0;
-
-  if (trajectory.size() <= window)
-    return 0;
-
-  trajectory_type::iterator itr1 = trajectory.begin();
-  trajectory_type::iterator itr2 = itr1 + window;
-  double diff;
-  bool found = false;
-
-  do {
-    diff = fabs(itr1->real_property("heading") - 
-     itr2->real_property("heading"));
-    if (fabs(diff - 180.0) < 2.0) {  // 2.0 is the fudge factor for 180
-      if (!found) {
-        ++ctr;
-      }
-      found = true;
-    } else {
-      found = false;
-    }
-    if (found) {
-      int leap = 
-       std::min(static_cast<int>(std::distance(itr2,trajectory.end())),5);
-  // The 5 above is related to how far away you want to go before you
-  // define another turnaround
-      itr1 += leap; itr2 += leap;
-    } else {
-      ++itr1; ++itr2;
-    }
-  } while (itr2 != trajectory.end());
-
-  return ctr;
-}
-
-double StraightFraction(trajectory_type &trajectory)
-{
-  int sum = 0;
-  const int min_straight_size = 5;
-
-  trajectory_type::iterator itr1 = trajectory.begin();
-  trajectory_type::iterator itr2;
-
-  do {
-  // The 2.0 below is related to how different the straightness can be from 180
-    itr2 = std::adjacent_find(itr1,trajectory.end(),
-     !boost::bind(std::less<double>(),
-     boost::bind(fabs,(boost::bind(TrajHeadingDifference,_1,_2))),2.0));
-    if (itr2 != trajectory.end()) ++itr2;
-    if ((itr2 - itr1) >= min_straight_size)
-      sum += (itr2 - itr1);
-    itr1 = itr2;
-  } while (itr2 != trajectory.end());
-
-  return static_cast<double>(sum)/static_cast<double>(trajectory.size());
-}
 
 double HeadingDifference(const double h2, const double h1)
 {
   return (h2 - h1) - 360.0 * int((h2-h1)/180.0);
 }
 
-double TrajHeadingDifference(const trajectory_point_type &t2, 
- const trajectory_point_type &t1)
+double TrajHeadingDifference(
+                             const trajectory_point_type &t2, 
+                             const trajectory_point_type &t1)
 {
   double h2 = t2.real_property("heading");
   double h1 = t1.real_property("heading");
@@ -88,3 +30,87 @@ double TrajHeadingDifference(const trajectory_point_type &t2,
   return HeadingDifference(h2,h1);
 }
 
+double AbsTrajHeadingDifference(trajectory_point_type const& t1, trajectory_point_type const& t2)
+{
+  return fabs(TrajHeadingDifference(t1, t2));
+}
+
+
+unsigned int TurnArounds(trajectory_type const &trajectory)
+{
+  const unsigned int window = 5;
+  unsigned int ctr = 0;
+
+  if (trajectory.size() <= window)
+    return 0;
+
+  trajectory_type::const_iterator itr1 = trajectory.begin();
+  trajectory_type::const_iterator itr2 = itr1 + window;
+  double diff;
+  bool found = false;
+
+  do {
+    diff = fabs(itr1->real_property("heading") - 
+                itr2->real_property("heading"));
+
+    if (fabs(diff - 180.0) < 2.0)
+      {  // 2.0 is the fudge factor for comparing against 180 degrees
+      if (!found)
+        {
+          ++ctr;
+        }
+      found = true;
+      }
+    else
+      {
+        found = false;
+      }
+
+    if (found)
+      {
+        int leap = 
+          std::min(static_cast<int>(std::distance(itr2,trajectory.end())),5);
+        // The 5 above is related to how far away you want to go before you
+        // define another turnaround
+        itr1 += leap; itr2 += leap;
+      }
+    else
+      {
+        ++itr1; ++itr2;
+      }
+  } while (itr2 != trajectory.end());
+
+  return ctr;
+}
+
+// ----------------------------------------------------------------------
+
+double StraightFraction(trajectory_type const& trajectory)
+{
+  int sum = 0;
+  const int min_straight_size = 5;
+
+  trajectory_type::const_iterator itr1 = trajectory.begin();
+  trajectory_type::const_iterator itr2;
+
+  do {
+    itr2 = 
+    // The 2.0 below is related to how different the straightness can be from 180
+    itr2 = std::adjacent_find(itr1,
+                              trajectory.end(),
+                              !boost::bind(std::less<double>(),
+                                           boost::bind(AbsTrajHeadingDifference,_1,_2), 2.0));
+    
+                                           // boost::bind(fabs,
+                                           //             (boost::bind(TrajHeadingDifference,_1,_2))),2.0));
+
+    if (itr2 != trajectory.end()) ++itr2;
+
+    if ((itr2 - itr1) >= min_straight_size)
+      sum += (itr2 - itr1);
+
+    itr1 = itr2;
+  } while (itr2 != trajectory.end());
+
+  return static_cast<double>(sum)/static_cast<double>(trajectory.size());
+}
