e64596ad8573a73e75e1ddfe238fdc4babb9517a
Ben Newton <bdnewto@sandia.gov>
2017-10-24 15:22:48 -0600

    Json works for python2 now.  Convert unicode to utf-8 strings.

--- tracktable/Python/tracktable/io/read_write_json.py
+++ tracktable/Python/tracktable/io/read_write_json.py
@@ -33,17 +33,45 @@
 tracktable.io.read_write_json - Read/Write a trajectory from/to a JSON file
 """
 
+import sys
 import json
 from tracktable.io.read_write_dictionary import trajectory_from_dictionary
 from tracktable.io.read_write_dictionary import dictionary_from_trajectory
 
-def trajectory_from_json(jsonString):
+# below by Mirec Miskuf from: https://stackoverflow.com/questions/956867/how-to-get-string-objects-instead-of-unicode-from-json
+def json_loads_byteified(json_text):
+    return _byteify(
+        json.loads(json_text, object_hook=_byteify),
+        ignore_dicts=True
+    )
+
+# below by Mirec Miskuf from: https://stackoverflow.com/questions/956867/how-to-get-string-objects-instead-of-unicode-from-json
+def _byteify(data, ignore_dicts = False):
+    # if this is a unicode string, return its string representation
+    if isinstance(data, unicode):
+        return data.encode('utf-8')
+    # if this is a list of values, return list of byteified values
+    if isinstance(data, list):
+        return [ _byteify(item, ignore_dicts=True) for item in data ]
+    # if this is a dictionary, return dictionary of byteified keys and values
+    # but only if we haven't already byteified it
+    if isinstance(data, dict) and not ignore_dicts:
+        return {
+            _byteify(key, ignore_dicts=True): _byteify(value, ignore_dicts=True)
+            for key, value in data.iteritems()
+        }
+    # if it's anything else, return it in its original form
+    return data
+
+def trajectory_from_json(json_string):
     """Returns a trajectory constructed from the given json string.
     Args:
        json: the json to convert into a trajectory
     """
-
-    return trajectory_from_dictionary(json.loads(jsonString))
+    if sys.version_info[0] < 3:
+        return trajectory_from_dictionary(json_loads_byteified(json_string))
+    else:
+        return trajectory_from_dictionary(json.loads(json_string))
 
 def json_from_trajectory(trajectory):
     """Returns a json string constructed from the given trajectory
