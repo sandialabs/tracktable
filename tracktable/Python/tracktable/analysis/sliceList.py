ee3a25f0ae1cf6fce35302460975c2e8bc74a7a2
pschrum <pschrum@sandia.gov>
2018-06-04 15:17:04 -0600

    Add __main__ tests to improve coverage, thus reliability.

--- tracktable/Python/tracktable/analysis/sliceList.py
+++ tracktable/Python/tracktable/analysis/sliceList.py
@@ -32,7 +32,10 @@ class sliceRange():
         return self.target[self.getSlice()]
 
     def __repr__(self):
-        return str(self.getSlice())
+        attribs = [x for x in self.__dict__ if 'target' not in x and 'iChanged' not in x]
+        atDict = {k : self.__dict__[k] for k in attribs}
+        retStr = str(self.getSlice()) + ' ' + str(atDict)
+        return retStr
 
 class SliceList(deque):
     '''
@@ -57,8 +60,13 @@ class SliceList(deque):
         self.target = TargetSequence
         self.overlap = Overlap + 1
         self.startDelta = RangeWidth - Overlap - 1
+        if 0 == self.startDelta:
+            raise ValueError("RangeWidth must be at least 2 greater than Overlap.")
         initialCount = len(self.target) // self.startDelta
         for r in range(0, 1+initialCount, self.startDelta+1):
+            if r >= len(self.target):
+                break
+
             end = r + self.startDelta + self.overlap
             self.append(sliceRange(self.target, r, end))
         stopEnd = self[-1].stop; targetLen = len(self.target)
@@ -66,6 +74,10 @@ class SliceList(deque):
             begin = self[-1].stop - self.overlap + 1
             self.append(sliceRange(self.target, begin, targetLen))
 
+        if self[-1].stop > targetLen:
+            self[-1].stop =  targetLen
+        if len(self) > 2 and self[-2].stop > targetLen:
+            self[-2].stop =  targetLen
         self.computeParams = computeParams
         self.computeDesiredParameters()
 
@@ -97,7 +109,10 @@ class SliceList(deque):
                 continue
 
 
-            pred = predicate(item, prev)
+            try:
+                pred = predicate(item, prev)
+            except AttributeError:
+                pred = False
             if pred:
                 item.start = prev.start
                 prev.delme = True
@@ -115,41 +130,80 @@ class SliceList(deque):
 
         self.computeDesiredParameters()
 
+    def __str__(self):
+        retStr = ''
+        for itm in self:
+            retStr = retStr +  str(itm) + '\n'
+        return retStr
+
 if __name__ == '__main__':
     import statistics, math
 
-    
+
     # Test 1
 
-    col1 = [1.2, 4.4, 0.2, -1.1, 8.9, 1.4, 1.5, 1.6, 1.4, 1.3, 1.45, 1.55]
-    def computeParameters(aSliceRange):
-        aSliceRange.mean = statistics.mean(aSliceRange.getSegment())
-        aSliceRange.stdDev = statistics.stdev(aSliceRange.getSegment())
+    if True:
+        col1 = [1.2, 4.4, 0.2, -1.1, 8.9, 1.4, 1.5, 1.6, 1.4, 1.3, 1.45, 1.55]
+        def computeParameters(aSliceRange):
+            aSliceRange.mean = statistics.mean(aSliceRange.getSegment())
+            aSliceRange.stdDev = statistics.stdev(aSliceRange.getSegment())
 
-    computeFunc = lambda each: computeParameters(each)
-    aSliceList = SliceList(col1, RangeWidth=5, Overlap=2,
-                           computeParams= computeFunc)
-    print(aSliceList)
-    for s in aSliceList.allSlices():
-        print(s)
-    for s in aSliceList.allSegments():
-        print(s)
-    for s in aSliceList.allCombined():
-        print(s)
+        computeFunc = lambda each: computeParameters(each)
+        aSliceList = SliceList(col1, RangeWidth=5, Overlap=2,
+                               computeParams= computeFunc)
+        print(aSliceList)
+        for s in aSliceList.allSlices():
+            print(s)
+        for s in aSliceList.allSegments():
+            print(s)
+        for s in aSliceList.allCombined():
+            print(s)
 
-    print(); print('Before Consolidating:')
-    print('Slice ID          Slice Mean   Slice StdDev')
-    for s in aSliceList.allSlices():
-        print(s, '  ', s.mean, '  ', s.stdDev)
+        print(); print('Before Consolidating:')
+        print('Slice ID          Slice Mean   Slice StdDev')
+        for s in aSliceList.allSlices():
+            print(s, '  ', s.mean, '  ', s.stdDev)
 
-    predicate = lambda a, b: math.isclose(a.stdDev, b.stdDev, abs_tol=0.025)
-    aSliceList.consolidateNodeIf(predicate= predicate)
-    print(); print('After Consolidating:')
+        predicate = lambda a, b: math.isclose(a.stdDev, b.stdDev, abs_tol=0.025)
+        aSliceList.consolidateNodeIf(predicate= predicate)
+        print(); print('After Consolidating:')
 
-    print('Slice ID          Slice Mean   Slice StdDev')
-    for s in aSliceList.allSlices():
-        print(s, '  ', s.mean, '  ', s.stdDev)
+        print('Slice ID          Slice Mean   Slice StdDev')
+        for s in aSliceList.allSlices():
+            print(s, '  ', s.mean, '  ', s.stdDev)
 
+    # Test 2
 
-    import sys
-    sys.exit(0)
+    secondList = [ (1.2, 0.2), (2.0, 0.1), (2.9, 0.3),
+                   (4.2, 0.2), (5.0, 0.1), (5.9, 0.3),
+                   (7.2, 0.2), (8.0, 0.1), (8.9, 0.3),
+                   (10.2, 0.2), (12.0, 0.1), (12.9, 0.3),
+                   (14.2, 0.2), (15.0, 0.1), (15.9, 0.3),
+                   (17.2, 0.2), (18.0, 0.1), (18.9, 0.3),
+                   (21.2, 0.2), (22.0, 0.1), (22.9, 0.3),
+                   (24.2, 0.2), (25.0, 0.1), (25.9, 0.3),
+                   (27.2, 0.2), (28.0, 0.1), (28.9, 0.3),
+                   (31.2, 0.2), (32.0, 0.1), (32.9, 0.3),
+                   (34.2, 0.2), (35.0, 0.1), (35.9, 0.3),
+                   (37.2, 0.2), (38.0, 0.1), (38.9, 0.3),
+                   ]
+
+    def computeParams(aSliceRange):
+        something = aSliceRange.getSegment()
+        myTp = type(something)
+        extractedList = [x[1] for x in something]
+        aSliceRange.ave = statistics.mean(extractedList)
+        aSliceRange.count = len(extractedList)
+        aSliceRange.stdDev = statistics.stdev(extractedList)
+
+    sliceList2 = SliceList(secondList, RangeWidth=5, Overlap=3,
+                           computeParams=computeParams)
+
+    print(); print("Test 2")
+    print(sliceList2)
+    print(len(sliceList2))
+
+    predicate = lambda a, b: math.isclose(a.stdDev, b.stdDev, abs_tol=0.01)
+    sliceList2.consolidateNodeIf(predicate)
+    print(sliceList2)
+    print(len(sliceList2))
