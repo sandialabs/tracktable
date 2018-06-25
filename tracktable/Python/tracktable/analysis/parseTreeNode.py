b0c41528cb567dadad23687362127235dde69300
pschrum <pschrum@sandia.gov>
2018-06-25 11:55:04 -0600

    Refactored ParseTreeNode to its own module.
    It is about to become a much more important class.

---
+++ tracktable/Python/tracktable/analysis/parseTreeNode.py
@@ -0,0 +1,103 @@
+import math
+
+class ParseTreeNode(list):
+    def __init__(self, sourceList, my_traj, associatedSlice=None,
+                 ndx=-1):
+        self.my_slice = associatedSlice
+        self.my_trajectory = my_traj
+        if ndx > -1:
+            self.index = ndx
+        for itm in sourceList:
+            self.append(itm)
+
+    @property
+    def start(self):
+        return self[0]
+
+    @property
+    def stop(self):
+        return self[1]
+
+    def _traj_get_time_str(self, idx):
+        return self.my_trajectory[idx].timestamp.strftime('%H:%M:%S')
+
+    def _traj_get_duration_str(self, start, stop):
+        duration = self.my_trajectory[stop].timestamp - \
+                   self.my_trajectory[start].timestamp
+        return str(duration)
+
+    def _traj_get_alt_str(self, idx):
+        return str(self.my_trajectory[idx].Z)
+
+    def _traj_get_mid_alt_str(self):
+        traj_mid_point_count = len(self.my_trajectory) // 2
+        return str(self.my_trajectory[traj_mid_point_count].Z)
+
+    @property
+    def description(self):
+        retList = list()
+        retString = ('Segment: Index: {3} Target Index: {0} - {1}\n'
+                     'Seg Type: {2}') \
+            .format(self.start, self.stop - 1,
+                    self.my_slice.DegreeOfCurve, self.index)
+        retList.append(retString)
+
+        retList.append('\nWhole Trajectory:')
+        retList.append('Start Time: {0}' \
+                       .format(self._traj_get_time_str(0)))
+        retList.append('End   Time: {0}' \
+                       .format(self._traj_get_time_str(-1)))
+        retList.append('Duration: {0}' \
+                       .format(self._traj_get_duration_str(0, -1)))
+
+        retList.append('\nStart Alt: {0}' \
+                       .format(self._traj_get_alt_str(0)))
+        retList.append('Mid Alt: {0}' \
+                       .format(self._traj_get_mid_alt_str()))
+        retList.append('End Alt: {0}' \
+                       .format(self._traj_get_alt_str(-1)))
+
+        return '\n'.join(retList)
+
+    def create_debug_report_string1(self):
+        pt1 = self.my_trajectory[self.start]
+        build_string = ''
+        if self.start == 0:
+            real_start = 1
+        else:
+            real_start = self.start
+            pt1 = self.my_trajectory[real_start - 1]
+
+        build_string += '{0},{1},'.format(self.index,
+                                          self.my_slice.DegreeOfCurve)
+        main_string = '{2},{3:.3f},{4:.3f},{5:.3f},{6:+.3f},' \
+                      '{7:+.3f},{8:+.3f},{9:.3f},' \
+                      '{10:.4f},{11:.1f},{12:.1f},' \
+                      '{13:.3f}\n'
+        build_string += main_string \
+            .format(self.index, self.my_slice.DegreeOfCurve,
+                    pt1.my_index, pt1.Y, pt1.X,
+                    math.fabs(pt1.arc.radius),
+                    pt1.arc.degreeCurveDeg, pt1.arc.deflection_deg,
+                    pt1.pt2pt.deflection_deg, pt1.pt2pt.distanceBack,
+                    pt1.pt2pt.distanceAhead, pt1.pt2pt.seconds_back,
+                    pt1.pt2pt.speed_back_mph,
+                    pt1.arc.radial_acceleration[0])
+        for pt1 in self.my_trajectory[real_start:self.stop - 2]:
+            build_string += ',,'
+            build_string += main_string \
+                .format(self.index, self.my_slice.DegreeOfCurve,
+                        pt1.my_index, pt1.Y, pt1.X,
+                        math.fabs(pt1.arc.radius),
+                        pt1.arc.degreeCurveDeg,
+                        pt1.arc.deflection_deg,
+                        pt1.pt2pt.deflection_deg,
+                        pt1.pt2pt.distanceBack,
+                        pt1.pt2pt.distanceAhead,
+                        pt1.pt2pt.seconds_back,
+                        pt1.pt2pt.speed_back_mph,
+                        pt1.arc.radial_acceleration[0])
+
+        build_string += ',,,,,,\n'
+        return build_string
+
--- tracktable/Python/tracktable/analysis/sliceList.py
+++ tracktable/Python/tracktable/analysis/sliceList.py
@@ -19,12 +19,15 @@ The term "sequence" is used instead of "collection" because it only makes
 
 from collections import deque
 # import statistics
-import math
-import tracktable.core.geomath as geomath
+from tracktable.analysis.parseTreeNode import ParseTreeNode
+# from tracktable.Python.tracktable.analysis.parseTreeNode import ParseTreeNode
 
 lavendar_color = 'FFB57EDC'
 red_color = 'FF0000FF'
 white_color = 'FFFFFFFF'
+
+
+
 
 class sliceRange():
     """
@@ -73,109 +76,6 @@ class sliceRange():
 
     def __len__(self):
         return self.stop - self.start
-
-
-class ParseTreeNode(list):
-    def __init__(self, sourceList, my_traj, associatedSlice=None,
-                 ndx=-1):
-        self.my_slice = associatedSlice
-        self.my_trajectory = my_traj
-        if ndx > -1:
-            self.index = ndx
-        for itm in sourceList:
-            self.append(itm)
-
-    @property
-    def start(self):
-        return self[0]
-
-    @property
-    def stop(self):
-        return self[1]
-
-    def _traj_get_time_str(self, idx):
-        return self.my_trajectory[idx].timestamp.strftime('%H:%M:%S')
-
-    def _traj_get_duration_str(self, start, stop):
-        duration = self.my_trajectory[stop].timestamp - \
-                   self.my_trajectory[start].timestamp
-        return str(duration)
-
-    def _traj_get_alt_str(self, idx):
-        return str(self.my_trajectory[idx].Z)
-
-    def _traj_get_mid_alt_str(self):
-        traj_mid_point_count = len(self.my_trajectory) // 2
-        return str(self.my_trajectory[traj_mid_point_count].Z)
-
-
-    @property
-    def description(self):
-        retList = list()
-        retString = ('Segment: Index: {3} Target Index: {0} - {1}\n'
-                     'Seg Type: {2}') \
-            .format(self.start, self.stop-1,
-                    self.my_slice.DegreeOfCurve, self.index)
-        retList.append(retString)
-
-        retList.append('\nWhole Trajectory:')
-        retList.append('Start Time: {0}' \
-                       .format(self._traj_get_time_str(0)))
-        retList.append('End   Time: {0}' \
-                       .format(self._traj_get_time_str(-1)))
-        retList.append('Duration: {0}' \
-                       .format(self._traj_get_duration_str(0, -1)))
-
-        retList.append('\nStart Alt: {0}' \
-                       .format(self._traj_get_alt_str(0)))
-        retList.append('Mid Alt: {0}' \
-                       .format(self._traj_get_mid_alt_str()))
-        retList.append('End Alt: {0}' \
-                       .format(self._traj_get_alt_str(-1)))
-
-        return '\n'.join(retList)
-
-    def create_debug_report_string1(self):
-        pt1 = self.my_trajectory[self.start]
-        build_string = ''
-        if self.start == 0:
-            real_start = 1
-        else:
-            real_start = self.start
-            pt1 = self.my_trajectory[real_start-1]
-
-        build_string += '{0},{1},'.format(self.index,
-                                          self.my_slice.DegreeOfCurve)
-        main_string = '{2},{3:.3f},{4:.3f},{5:.3f},{6:+.3f},' \
-                      '{7:+.3f},{8:+.3f},{9:.3f},' \
-                      '{10:.4f},{11:.1f},{12:.1f},' \
-                      '{13:.3f}\n'
-        build_string += main_string \
-            .format(self.index,self.my_slice.DegreeOfCurve,
-                    pt1.my_index, pt1.Y, pt1.X,
-                    math.fabs(pt1.arc.radius),
-                    pt1.arc.degreeCurveDeg, pt1.arc.deflection_deg,
-                    pt1.pt2pt.deflection_deg, pt1.pt2pt.distanceBack,
-                    pt1.pt2pt.distanceAhead, pt1.pt2pt.seconds_back,
-                    pt1.pt2pt.speed_back_mph,
-                    pt1.arc.radial_acceleration[0])
-        for pt1 in self.my_trajectory[real_start:self.stop-2]:
-            build_string += ',,'
-            build_string += main_string \
-                .format(self.index, self.my_slice.DegreeOfCurve,
-                        pt1.my_index, pt1.Y, pt1.X,
-                        math.fabs(pt1.arc.radius),
-                        pt1.arc.degreeCurveDeg,
-                        pt1.arc.deflection_deg,
-                        pt1.pt2pt.deflection_deg,
-                        pt1.pt2pt.distanceBack,
-                        pt1.pt2pt.distanceAhead,
-                        pt1.pt2pt.seconds_back,
-                        pt1.pt2pt.speed_back_mph,
-                        pt1.arc.radial_acceleration[0])
-
-        build_string += ',,,,,,\n'
-        return build_string
 
 
 class SliceList(deque):
@@ -280,9 +180,10 @@ class SliceList(deque):
 
         adjustBack = int(self.overlap // 2.0) - 1
         adjustFront = int(self.overlap // 2.0)
-        leafList = [ParseTreeNode([sl.start + adjustFront, sl.stop - adjustBack],
-                                  self.target, sl, index)
-                    for index, sl in enumerate(slices)]
+        leafList = [
+            ParseTreeNode([sl.start + adjustFront, sl.stop - adjustBack],
+                          self.target, sl, index)
+            for index, sl in enumerate(slices)]
 
         for a_leaf, a_slice in zip(leafList, slices):
             a_leaf.color = a_slice.color
