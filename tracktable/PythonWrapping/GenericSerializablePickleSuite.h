521ba2d257c77de387dcb84ffb33813735d82c9b
Andy Wilson <atwilso@sandia.gov>
2019-06-11 16:59:15 -0600

    Use Python bytes instead of str
    Big fix: instead of trying to wedge arbitrary binary data into a
    Python str() object with Boost.Python, just drop down to the
    lower-level C API and use bytes(). This breaks compatibility with
    Python 2, but since we're disclaiming Python 2 support as of release
    1.2 this bothers me less than it might.
    
    I've also cleaned up the code and moved it into smaller functions so
    that it's easier to read.  Thank you, Chris Riesbeck, wherever you
    are.

--- tracktable/PythonWrapping/GenericSerializablePickleSuite.h
+++ tracktable/PythonWrapping/GenericSerializablePickleSuite.h
@@ -66,41 +66,102 @@ public:
     native_object_t const& native_object = boost::python::extract<native_object_t const&>(object_to_pickle);
     
     archive << native_object;
-    return boost::python::make_tuple(BP_STRING_TYPE(outbuf.str()),
+    // Because Boost.Python doesn't provide a way to create a Python
+    // bytes object we have to drop through to the C API.
+    boost::python::object archive_bytes(
+      boost::python::handle<>(
+        PyBytes_FromString(outbuf.str().c_str())
+      )
+    );
+    
+    return boost::python::make_tuple(archive_bytes,
                                      object_to_pickle.attr("__dict__"));
-
   }                                        
-  
+
   static void setstate(boost::python::object& object_to_restore, boost::python::tuple state)
   {
     using boost::python::dict;
     using boost::python::extract;
-    using boost::python::str;
-    using boost::python::tuple;
+    
+    check_tuple_size(state, 2);
+    check_for_bytes(state[0]);
+    check_for_dict(state[1]);
 
-    if (len(state) != 2)
-      {
-      PyErr_SetObject(PyExc_ValueError,
-                      ("Expected 2-item tuple in call to __setstate__; got %s" % state).ptr());
-      boost::python::throw_error_already_set();
-      }
-    
-    // Restore the object's __dict__ attribute
-    dict d = extract<dict>(object_to_restore);
-    d.update(state[1]);
-    
-    // Restore the object's C++ attributes
-    native_object_t& native_object = extract<native_object_t&>(object_to_restore);
-    
-    BP_STRING_TYPE state_as_python_str((boost::python::extract<BP_STRING_TYPE>(state[0])));
-    std::string state_as_string((boost::python::extract<std::string>(state_as_python_str)));
-    std::istringstream inbuf(state_as_string);
+    // We have already checked to make sure this is a bytes object.
+    // Something might still go wrong extracting the bytes as a C
+    // string, though.
+    boost::python::object bytes_object = state[0];
+    PyObject* bytes = bytes_object.ptr();
+    const char* bytes_as_c_string = PyBytes_AsString(bytes);
+    check_extracted_string(bytes_as_c_string);
+
+    std::string archive_data(bytes_as_c_string);
+    std::istringstream inbuf(archive_data);
     boost::archive::binary_iarchive archive(inbuf);
     
+    native_object_t& native_object = extract<native_object_t&>(object_to_restore);
     archive >> native_object;
   }
   
   static bool getstate_manages_dict() { return true; }
+
+  // ----------------------------------------------------------------------
+  // Utility functions below here
+  // ----------------------------------------------------------------------
+  
+  // Does this tuple conform to our expectations?
+  static void check_tuple_size(boost::python::object tuple,
+                               int expected_size)
+  {
+    using boost::python::len;
+    
+    if (len(tuple) != expected_size)
+      {
+      PyErr_SetObject(PyExc_ValueError,
+                      ("Expected %s-item tuple; got %s" % 
+                       boost::python::make_tuple(len(tuple), expected_size)).ptr());
+      boost::python::throw_error_already_set();
+      }
+  }
+
+  static void check_extracted_string(const char *extracted_string)
+  {
+    if (!extracted_string)
+      {
+      PyErr_SetObject(PyExc_ValueError,
+                      boost::python::str("String data extracted from bytes object is null").ptr());
+      boost::python::throw_error_already_set();
+      }
+  }
+  
+  static void check_for_dict(boost::python::object maybe_dict)
+  {
+    using boost::python::dict;
+    using boost::python::extract;
+    
+    extract<dict> dict_check(maybe_dict);
+    if (! dict_check.check())
+      {
+      PyErr_SetObject(PyExc_TypeError,
+                      ("Expected dict as first element of state: got %s" % maybe_dict).ptr());
+      boost::python::throw_error_already_set();
+      }
+  }
+
+  static void check_for_bytes(boost::python::object maybe_bytes)
+  {
+    using boost::python::dict;
+    using boost::python::extract;
+    
+    PyObject* bytes = maybe_bytes.ptr();
+    if (!PyBytes_Check(bytes))
+      {
+      PyErr_SetObject(PyExc_ValueError,
+                      ("Expected bytes() object in call to __setstate__; got %s" % 
+                       maybe_bytes).ptr());
+      boost::python::throw_error_already_set();
+      }
+  }
 };
 
 } } // namespace tracktable::python_wrapping
