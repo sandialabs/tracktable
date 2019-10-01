fa69a5641584824acfca19f7e60412416a5decf0
Andy Wilson <atwilso@sandia.gov>
2019-10-01 01:45:53 -0600

    Bump version to 1.2.0; add Kat Ward to authors

--- tracktable/Python/tracktable/__init__.py
+++ tracktable/Python/tracktable/__init__.py
@@ -40,13 +40,13 @@ in kilometers.
 
 # \defgroup Tracktable_Python Python components of Tracktable
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 __title__ = "Tracktable"
 __description__ = "Trajectory Analysis and Visualization"
 __url__ = "https://tracktable.sandia.gov"
 __uri__ = __url__
 __doc__ = __description__ + " <" + __uri__ + ">"
-__author__ = "Andy Wilson, Danny Rintoul, Chris Valicka, Ben Newton, Paul Schrum, Phil Baxley"
+__author__ = "Andy Wilson, Danny Rintoul, Chris Valicka, Ben Newton, Paul Schrum, Phil Baxley, Kat Ward"
 __maintainer__ = "Andy Wilson and Phil Baxley"
 __license__ = "BSD"
 __copyright__ = "Copyright (c) 2014-2019 National Technology and Engineering Solutions of Sandia, LLC."
