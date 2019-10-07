552c2450ed9c15c268b501da43c4ea96976c7ad2
Andy Wilson <atwilso@sandia.gov>
2019-10-07 17:22:50 -0600

    Rename TT.source.scatter to TT.source.point
    This is a better name for the module.  Right now it contains two
    functions for generating random points.
    
    Also changed the generate_heatmap_sample_data notebook to match.

--- tracktable/Python/tracktable/source/point.py
+++ tracktable/Python/tracktable/source/point.py
@@ -34,26 +34,63 @@ tracktable.source.scatter - Ways to generate points around a seed point
 
 from __future__ import division, print_function, absolute_import
 
-import datetime
 import math
 import random
-
-from tracktable.core import geomath
 from six.moves import range
 
 
-def linear_falloff_scattered_points(seed_point,
-                                    num_points=100,
-                                    max_radius_km=100):
+def random_box_uniform(min_corner, max_corner, num_points=100):
+    """Generate points uniformly within a bounding box
+
+    This will generate points according to a uniform random 
+    distribution within a specified bounding box.
+
+    Note that the uniformity is with respect to each dimension,
+    not the underlying metric.  You will only get spatially uniform
+    points if (1) the bounding box is square and (2) the coordinate 
+    system is isotropic with respect to the underlying surface.  
+    Cartesian coordinate systems are such.  Geographic coordinates
+    (longitude/latitude) are not -- the width of 1 degree of
+    longitude changes as a function of latitude.
+
+    Arguments:
+        min_corner (Tracktable point type): lower left / SW corner of 
+           bounding box
+        max_corner (Tracktable point type): upper right / NE corner of
+           bounding box
+        num_points (int): How many points you want
+    Returns:
+        Iterable of points
+    """
+
+    num_coords = len(min_corner)
+    point_type = min_corner.__class__
+    def generate_point():
+        new_coords = [random.uniform(min_corner[i], max_corner[i])
+                      for i in range(num_coords)]
+        return point_type(*new_coords)
+
+    return (generate_point() for j in range(num_points))
+
+
+def random_circle_linear_falloff(seed_point,
+                                 num_points=100,
+                                 max_radius_km=100):
     """Generate points within a certain radius of a seed point
 
     Point density falls off linearly with distance from the seed
     point.
 
+    NOTE: Right now this function is specialized for terrestrial
+          coordinates.  That will be fixed in an upcoming point
+          release.
+
     Args:
-       seed_point (tracktable.domain.terrestrial.BasePoint): Center of point cloud
+       seed_point (tracktable.domain.terrestrial.BasePoint): Center of 
+           point cloud
        num_points (integer, default 100): Number of points to generate
-       max_radius_km (float, non-negative): How far the points can be from the center
+       max_radius (float, non-negative): How far the points can be from 
+           the center (default 100km)
 
     Returns:
        Iterable of points of same type as seed point
