cabba6393831955997e5b02d83e0a1fb1c9e7800
Andy Wilson <atwilso@sandia.gov>
2020-04-24 23:27:31 -0600

    Convex hull aspect ratio is long / short, not short / long
    Oops -- I had these backwards.
    
    I also added explicit checks for "is short == 0?" and
    "is the result NaN?" in order to return 0 in both cases.  There is
    an argument to be made that NaN is a correct answer in the degenerate
    case but 0 is a more useful answer.

--- tracktable/Core/detail/implementations/ConvexHullAspectRatioTerrestrial.h
+++ tracktable/Core/detail/implementations/ConvexHullAspectRatioTerrestrial.h
@@ -102,13 +102,19 @@ struct compute_convex_hull_aspect_ratio<
               << long_axis;
 #endif
 
-    if (std::abs(long_axis) < 1e-5)
+    if (std::abs(short_axis) < 1e-5)
       {
-      return 0;
+        return 0;
       }
     else
       {
-      return short_axis / long_axis;
+        double result = long_axis / short_axis;
+        if (std::isnan(result)) 
+        {
+          return 0;
+        } else {
+          return result;
+        }
       }
     }
 };
