d24da438f39cc6e33b00e22c63a9c56db6ba0c9a
Andy Wilson <atwilso@sandia.gov>
2020-05-15 21:05:11 -0600

    Bring Python bindings into tracktable.core.log
    It's there.  I also added a very simple test case that makes sure
    we can call set_log_level() without the code dying.

---
+++ tracktable/Python/tracktable/core/tests/test_log_level.py
@@ -0,0 +1,57 @@
+#
+# Copyright (c) 2014-2020 National Technology and Engineering
+# Solutions of Sandia, LLC. Under the terms of Contract DE-NA0003525
+# with National Technology and Engineering Solutions of Sandia, LLC,
+# the U.S. Government retains certain rights in this software.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import print_function, division, absolute_import
+
+import sys
+
+import logging
+import tracktable.core.log
+
+# At present this is a smoke test rather than a function test.  We'll
+# add a function test once we build a bridge from Boost logging
+# to Python logging.
+
+
+def test_log_level():
+    tracktable.core.log.set_log_level(logging.INFO)
+    logging.getLogger(__name__).debug("This message should not print.")
+    logging.getLogger(__name__).info("This message should print.")
+    logging.getLogger(__name__).error("This isn't an error, just a test of the logger.")
+    return 0
+
+
+def main():
+    num_errors = test_log_level()
+    return num_errors
+
+
+if __name__ == '__main__':
+    sys.exit(main())
