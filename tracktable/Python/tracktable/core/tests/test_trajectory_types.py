3917defc6766aa0323956a5f1495709e8b92e45a
pbaxley <pbaxley@sandia.gov>
2020-07-30 08:53:15 -0600

    Feature/203 trajectory clone

--- tracktable/Python/tracktable/core/tests/test_trajectory_types.py
+++ tracktable/Python/tracktable/core/tests/test_trajectory_types.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2014-2018 National Technology and Engineering
+# Copyright (c) 2014-2020 National Technology and Engineering
 # Solutions of Sandia, LLC. Under the terms of Contract DE-NA0003525
 # with National Technology and Engineering Solutions of Sandia, LLC,
 # the U.S. Government retains certain rights in this software.
@@ -91,7 +91,16 @@ def test_trajectory():
     round_trip = [ boston, miami, san_francisco, seattle, boston_return ]
 
     my_trajectory = TerrestrialTrajectory.from_position_list(round_trip)
-
+    
+    print("Testing clone")
+    copied_trajectory = my_trajectory.clone()
+    for i in range(0, len(my_trajectory)):
+        error_count += verify_point(my_trajectory[i], copied_trajectory[i], "Cloning Test")
+    copied_trajectory[0][0] = 0.0
+    if my_trajectory[0][0] == 0.0:
+        sys.stderr.write('ERROR: Cloned trajectory not a deep copy')
+        error_count += 1        
+    
     print("Testing from_position_list")
     if len(my_trajectory) != 5:
         sys.stderr.write('ERROR: Expected length of trajectory to be 5 points but it was {}\n'.format(len(my_trajectory)))
