92ca8ba84ad1de12fc0bfa4aeddbc4b45c3a44c1
Michael J Fadem <mjfadem@sandia.gov>
2022-03-31 07:58:17 -0600

    Resolve "Add Project Badges To Repo"

--- ci/linux/dockerenv/docker-build.sh
+++ ci/linux/dockerenv/docker-build.sh
@@ -7,8 +7,6 @@ cp ../linux_ci_environment.yml .
 
 docker build -t ${IMAGE_NAME}:${IMAGE_TAG} \
     --build-arg CI_REGISTRY=${CI_REGISTRY} \
-    --build-arg HTTPS_PROXY=${SNL_HTTPS_PROXY} \
-    --build-arg HTTP_PROXY=${SNL_HTTP_PROXY} \
     .
 
 if [[ "${TAG_IMAGE_LATEST}" == "true" ]]; then
