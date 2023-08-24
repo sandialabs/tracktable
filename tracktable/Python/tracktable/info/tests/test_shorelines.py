15372630af9687b6c9320d7283e43e45ccf53a5c
Andy Wilson <atwilso@sandia.gov>
2023-08-24 16:36:34 -0600

    Bump copyright date to 2023
    Update copyright date ranges in all of Tracktable core to be
    2014-2023.

--- tracktable/Python/tracktable/info/tests/test_shorelines.py
+++ tracktable/Python/tracktable/info/tests/test_shorelines.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014-2022, National Technology & Engineering Solutions of
+# Copyright (c) 2014-2023, National Technology & Engineering Solutions of
 #   Sandia, LLC (NTESS).
 # All rights reserved.
 #
