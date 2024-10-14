8ebd83df0a93a0182439a3fb73f4d875bd4baa24
Andrew T. Wilson <atwilso@sandia.gov>
2024-10-14 06:33:03 +0000

    Adds CMake `add_cpp_test()` function in CplusplusTest.cmake.  This packages up all the individual steps we used to do separately to add C++ tests: add the executable, move it to a folder, set its libraries, set the test name, and set the command to run.

--- CMake/Modules/CplusplusTest.cmake
+++ CMake/Modules/CplusplusTest.cmake
@@ -28,7 +28,88 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-# Function to set up and run a Python test using 'python -m foo'
+function(add_cpp_test)
+  # Include this option if the test uses Catch2
+  set(options CATCH2 MY_OPTION)
+  # SOURCE is 'my_test.cpp', NAME is 'C_MyTest'
+  set(single_value_arguments SOURCE NAME WORKING_DIRECTORY TARGET)
+  set(multiple_value_arguments LIBRARIES COMMAND SOURCES)
+  cmake_parse_arguments(PARSE_ARGV 0 arg
+    "${options}" "${single_value_arguments}" "${multiple_value_arguments}"
+    )
+
+  if (NOT (arg_SOURCE OR arg_SOURCES))
+    message(ERROR "You must specify either SOURCE or SOURCES with add_cpp_test().")
+  endif()
+
+  if (arg_SOURCE AND arg_SOURCES)
+    message(ERROR "You can use either SOURCE or SOURCES but not both with add_cpp_test().")
+  endif ()
+
+  if (arg_SOURCES AND (NOT arg_TARGET))
+    message(ERROR "You must specify TARGET when you specify SOURCES.")
+  endif()
+
+  # Add the binary target for the test
+
+  # Single-source-file version
+  if (arg_SOURCE)
+    if (NOT arg_TARGET)
+      # Get the basename of the source file for use as the target name
+      cmake_path(GET arg_SOURCE STEM target_name)
+    else ()
+      set(target_name ${arg_TARGET})
+    endif()
+    add_executable(${target_name} ${arg_SOURCE})
+  endif (arg_SOURCE)
+
+  # Multiple-source-file version
+  if (arg_SOURCES)
+    set(target_name ${arg_TARGET})
+    add_executable(${target_name} ${arg_SOURCES})
+  endif (arg_SOURCES)
+
+
+  target_link_libraries(${target_name} ${arg_LIBRARIES})
+
+  # Have Visual Studio put tests in a subfolder in the IDE
+  set_property(TARGET ${target_name} PROPERTY FOLDER "Tests")
+
+  # Tell CMake about the test
+  if (arg_CATCH2)
+    if (NOT arg_COMMAND)
+      set(arg_COMMAND ${target_name} -r console)
+    endif ()
+    if (arg_WORKING_DIRECTORY)
+      add_test(
+        NAME ${arg_NAME}
+        COMMAND ${arg_COMMAND}
+        #COMMAND ${target_name} -r console
+        WORKING_DIRECTORY ${arg_WORKING_DIRECTORY}
+        COMMAND_EXPAND_LISTS
+      )
+    else (arg_WORKING_DIRECTORY)
+      add_test(
+        NAME ${arg_NAME}
+        COMMAND ${arg_COMMAND}
+        # COMMAND ${target_name} -r console
+        COMMAND_EXPAND_LISTS
+      )
+    endif (arg_WORKING_DIRECTORY)
+  else (arg_CATCH2)
+    if (NOT arg_COMMAND)
+      set(arg_COMMAND "${target_name}")
+    endif ()
+    add_test(
+      NAME ${arg_NAME}
+      COMMAND ${arg_COMMAND}
+      COMMAND_EXPAND_LISTS
+    )
+  endif (arg_CATCH2)
+endfunction (add_cpp_test)
+
+
+# This is outdated and will go away once I'm certain the new function works.
 
 function(add_catch2_test test_name module_to_run)
 
