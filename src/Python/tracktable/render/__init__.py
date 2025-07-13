4979bde73e0c66491bf7e79cba8100064658362a
Andrew (Andy) T. Wilson <atwilso@sandia.gov>
2025-07-13 05:04:51 +0000

    Resolve "Move folium_proxy to tracktable.render"

--- src/Python/tracktable/render/__init__.py
+++ src/Python/tracktable/render/__init__.py
@@ -35,7 +35,7 @@ regular Cartesian space or on a (world) map.  The 'maps' module
 provides a friendly way to get a map of various parts of the world.
 """
 
-from tracktable.render.backends.folium_proxy import (
+from tracktable.render.folium_proxy import (
     folium_proxy_name,
     folium_proxy_enabled,
     set_folium_proxy_name,
